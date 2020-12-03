5cddfaf2c8955c98b8068f13a8a4f6ff37b00318
Michael Sena <mike@msena.com>
2020-12-03 17:50:29 -0500

    Day 2 part 1 complete

---
+++ Sources/advent-of-code/2020/Day2.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+enum PasswordPolicy {
+    case characterRequirement(Character, Int, Int)    
+}
+
+extension String {
+    func isValidPassword(with policy: PasswordPolicy) -> Bool {
+        switch policy {
+        case .characterRequirement(let character, let min, let max):
+            let count = filter { $0 == character }.count
+            return (count >= min) && (count <= max)
+        }
+    }
+}
+    
+func parse(input: String) -> (PasswordPolicy, String) {
+
+    let components = input.components(separatedBy: " ")
+
+    let countComponent = components[0]
+    let character = components[1].first!
+    let password = components[2]
+
+    let countComponents = countComponent.components(separatedBy: "-")
+    let min = Int(countComponents[0])!
+    let max = Int(countComponents[1])!
+    
+    return (.characterRequirement(character, min, max), password)
+}
--- Sources/advent-of-code/main.swift
+++ Sources/advent-of-code/main.swift
@@ -1,2 +1,9 @@
 //print(day1Part1())
-print(day1Part2())
+// print(day1Part2())
+print(day2Input
+    .split { $0.isNewline }
+    .map { String($0) }
+    .map { parse(input: $0) }
+    .filter { (policy, password) in 
+        return password.isValidPassword(with:policy) 
+        }.count)
