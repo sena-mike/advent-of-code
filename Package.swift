c749397bae663f576cb935eddc200f312056a5a7
Michael Sena <mike@msena.com>
2020-12-03 17:11:12 -0500

    Day 1 part 2 complete

--- Package.swift
+++ Package.swift
@@ -7,14 +7,17 @@ let package = Package(
     name: "advent-of-code",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-algorithms", from: "0.0.1"),
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "advent-of-code",
-            dependencies: []),
+            dependencies: [
+            .product(name: "Algorithms", package: "swift-algorithms")
+            ]),
         .testTarget(
             name: "advent-of-codeTests",
             dependencies: ["advent-of-code"]),
