0163249354196362fbbb54b20783e224fde21b6c
Michael Sena <mike@msena.com>
2021-01-12 09:23:23 -0500

    some untested changes

--- Sources/advent-of-code/main.swift
+++ Sources/advent-of-code/main.swift
@@ -1,9 +1,14 @@
+// Day 1
 //print(day1Part1())
 // print(day1Part2())
-print(day2Input
-    .split { $0.isNewline }
-    .map { String($0) }
-    .map { parsePositionRequirement(input: $0) }
-    .filter { (policy, password) in 
-        return password.isValidPassword(with:policy) 
-        }.count)
+
+// Day 2
+// print(day2Input
+//     .split { $0.isNewline }
+//     .map { String($0) }
+//     .map { parsePositionRequirement(input: $0) }
+//     .filter { (policy, password) in 
+//         return password.isValidPassword(with:policy) 
+//         }.count)
+
+print(computeHits(on: day3Map, mapSize: 32, slope: (3, 1)))
